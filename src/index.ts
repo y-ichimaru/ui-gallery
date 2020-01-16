950e3dd1ebf9d2e56cf2c5bdbececdd0f5ff021d
r.okada <r.okada@scene-live.com>
2020-01-16 18:27:12 +0900

    [add] storybookのビルドディレクトリ作成

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
  * @package UI-Gallery
  * @description 汎用のVue Component群をまとめたパッケージです．
  */
-import Okada from "@/components/HelloWorld.vue";
+import Okada from "./components/HelloWorld.vue";
 import "./style.scss";
+
 export
 {
     Okada
--- src/stories/0-Welcome.stories.js
+++ src/stories/0-Welcome.stories.js
@@ -1,14 +1,15 @@
 import {linkTo} from "@storybook/addon-links";
-import {HelloWorld} from "../components";
 import {storiesOf} from "@storybook/vue";
 import {withInfo} from "storybook-addon-vue-info";
 import {action} from "@storybook/addon-actions";
 
+import {Okada} from "../index.ts";
+
 storiesOf("岡田シリーズ", module)
     .add("岡田のボタン", () => ({
-        components: {HelloWorld},
+        components: {Okada},
         template: `
-        <HelloWorld/>
+        <Okada/>
         `,
         methods: {action: action("button-clicked")}
     }), {
